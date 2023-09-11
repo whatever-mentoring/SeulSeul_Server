1972f63d44f44ba1cc7c5af9ab9f435c4dae63e4
jooda00 <dragon2000a@gmail.com>
2023-09-11 17:00:26 +0900

    git init

---
+++ src/main/java/com/seulseul/seulseul/SeulseulApplication.java
@@ -0,0 +1,13 @@
+package com.seulseul.seulseul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeulseulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeulseulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seulseul/seulseul/SeulseulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seulseul.seulseul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeulseulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
