a6b18de46f5a8410bd8b34e73a83da8c318f24b9
jooda00 <dragon2000a@gmail.com>
2023-09-13 21:52:53 +0900

    feat: BaseRoute controller
    - BaseRouteStartReqDto 사용해서 받아오기

--- src/main/java/com/seulseul/seulseul/controller/baseRoute/BaseRouteController.java
+++ src/main/java/com/seulseul/seulseul/controller/baseRoute/BaseRouteController.java
@@ -1,4 +1,25 @@
 package com.seulseul.seulseul.controller.baseRoute;
 
+import com.seulseul.seulseul.dto.baseRoute.BaseRouteDto;
+import com.seulseul.seulseul.dto.baseRoute.BaseRouteStartReqDto;
+import com.seulseul.seulseul.service.baseRoute.BaseRouteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BaseRouteController {
+    private final BaseRouteService baseRouteService;
+
+    public BaseRouteController(BaseRouteService baseRouteService) {
+        this.baseRouteService = baseRouteService;
+    }
+
+    @PostMapping("/v1/start")
+    public ResponseEntity<BaseRouteDto> getStartCoordination(@RequestBody BaseRouteStartReqDto dto) {
+        baseRouteService.getStartCoordination(dto);
+        return new ResponseEntity<>(new BaseRouteDto(dto), HttpStatus.OK);
+    }
 }
--- src/main/java/com/seulseul/seulseul/dto/baseRoute/BaseRouteDto.java
+++ src/main/java/com/seulseul/seulseul/dto/baseRoute/BaseRouteDto.java
@@ -14,4 +14,13 @@ public class BaseRouteDto {
     private int endStation;
     private String firstStation;
     private String lastStation;
+    private double startX;
+    private double startY;
+    private String dayInfo;
+
+    public BaseRouteDto(BaseRouteStartReqDto dto) {
+        this.startX = dto.getStartX();
+        this.startY = dto.getStartY();
+        this.dayInfo = dto.getDayInfo();
+    }
 }
