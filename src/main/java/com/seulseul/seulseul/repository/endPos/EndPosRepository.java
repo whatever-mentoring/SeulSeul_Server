2585cf03232f875576706922303607ee29843ba9
seoyeon0201 <psy0201123@gmail.com>
2023-09-14 15:23:04 +0900

    Merge feature/destination

--- src/main/java/com/seulseul/seulseul/repository/endPos/EndPosRepository.java
+++ src/main/java/com/seulseul/seulseul/repository/endPos/EndPosRepository.java
@@ -1,4 +1,7 @@
 package com.seulseul.seulseul.repository.endPos;
 
-public interface EndPosRepository {
+import com.seulseul.seulseul.entity.endPos.EndPos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EndPosRepository extends CrudRepository<EndPos, Long> {
 }
--- src/main/java/com/seulseul/seulseul/service/baseRoute/BaseRouteService.java
+++ src/main/java/com/seulseul/seulseul/service/baseRoute/BaseRouteService.java
@@ -1,8 +1,11 @@
 package com.seulseul.seulseul.service.baseRoute;
 
 import com.seulseul.seulseul.dto.baseRoute.BaseRouteDto;
+import com.seulseul.seulseul.dto.endPos.EndPosDto;
 import com.seulseul.seulseul.entity.baseRoute.BaseRoute;
+import com.seulseul.seulseul.entity.endPos.EndPos;
 import com.seulseul.seulseul.repository.baseRoute.BaseRouteRepository;
+import com.seulseul.seulseul.repository.endPos.EndPosRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +16,28 @@ import org.springframework.stereotype.Service;
 public class BaseRouteService {
 
     private final BaseRouteRepository baseRouteRepository;
+    private final EndPosRepository endPosRepository;
+
     @Transactional
     public BaseRouteDto saveDest(BaseRouteDto form) {
-        BaseRoute domain = baseRouteRepository.save(form.toEntity(form));
-        BaseRouteDto dto = domain.toDto(domain);
+        BaseRoute entity = baseRouteRepository.save(form.toEntity(form));
+        BaseRouteDto dto = entity.toDto(entity);
+        return dto;
+    }
+
+    @Transactional
+    public BaseRouteDto getFromEndPos(Long id) {
+        //1. endPos 값 가져오기
+        EndPos endPos = endPosRepository.findById(id).orElse(null);
+        EndPosDto endPosDto = endPos.toDto(endPos);
+
+        //2. 원하는 위경도 값만 가져와 저장
+        BaseRouteDto dto = new BaseRouteDto();
+        dto.setId(endPos.getId());
+        dto.setEndx(endPosDto.getEndx());
+        dto.setEndy(endPosDto.getEndy());
+        baseRouteRepository.save(dto.toEntity(dto));
+
         return dto;
     }
 }
