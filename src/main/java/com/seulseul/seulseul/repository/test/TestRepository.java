b4842dc748e9269eebc1703a15d34b46c3273804
jooda00 <dragon2000a@gmail.com>
2023-09-15 17:29:16 +0900

    delete: test 삭제
    - 네트워크 세팅 완료되어서 test 삭제

--- src/main/java/com/seulseul/seulseul/repository/test/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seulseul.seulseul.repository.test;
-
-import com.seulseul.seulseul.entity.test.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
