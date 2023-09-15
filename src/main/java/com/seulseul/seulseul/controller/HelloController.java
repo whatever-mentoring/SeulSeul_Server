a4e0e193bf478af7e5812ad8be1b66bb4d89f744
jooda00 <dragon2000a@gmail.com>
2023-09-15 18:34:08 +0900

    feat: HelloController
    - conflict 방지를 위해서

--- src/main/java/com/seulseul/seulseul/controller/HelloController.java
+++ src/main/java/com/seulseul/seulseul/controller/HelloController.java
@@ -1,13 +1,8 @@
 package com.seulseul.seulseul.controller;
 
-import com.seulseul.seulseul.dto.baseRoute.BaseRouteDto;
-import com.seulseul.seulseul.dto.baseRoute.BaseRouteStartReqDto;
-import com.seulseul.seulseul.dto.test.TestDto;
 import com.seulseul.seulseul.dto.transferInfo.TransferInfoDto;
 import com.seulseul.seulseul.entity.ApiKey;
-import com.seulseul.seulseul.entity.test.Test;
 import com.seulseul.seulseul.service.baseRoute.BaseRouteService;
-import com.seulseul.seulseul.service.test.TestService;
 import com.seulseul.seulseul.service.transferInfo.TransferInfoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +19,11 @@ public class HelloController {
     private final ApiKey apiService;
     private final BaseRouteService baseRouteService;
     private final TransferInfoService transferInfoService;
-    private final TestService testService;
 
-    public HelloController(ApiKey apiService, BaseRouteService baseRouteService, TransferInfoService transferInfoService, TestService testService) {
+    public HelloController(ApiKey apiService, BaseRouteService baseRouteService, TransferInfoService transferInfoService) {
         this.apiService = apiService;
         this.baseRouteService = baseRouteService;
         this.transferInfoService = transferInfoService;
-        this.testService = testService;
     }
 /*
     @GetMapping("/hello")
@@ -38,14 +31,4 @@ public class HelloController {
         return new ResponseEntity(baseRouteService.getStationID(), HttpStatus.OK);
     }*/
 
-    @GetMapping("/trans")
-    public ResponseEntity<TransferInfoDto> getTransferInfo() throws IOException {
-        return new ResponseEntity<>(transferInfoService.getTransferInfo(), HttpStatus.OK);
-    }
-
-    @PostMapping("/v1/test")
-    public ResponseEntity<TestDto> saveDto(@RequestBody TestDto dto) {
-        testService.save(dto);
-        return new ResponseEntity<TestDto>(dto, HttpStatus.OK);
-    }
 }
