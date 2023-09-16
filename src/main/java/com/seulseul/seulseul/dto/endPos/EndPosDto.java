7145e265552476eec2122728e6acecbea8dec642
jooda00 <dragon2000a@gmail.com>
2023-09-16 16:26:34 +0900

    fix: 대문자로 변경
    - endpos dto 변수 대문자로 변경

--- src/main/java/com/seulseul/seulseul/dto/endPos/EndPosDto.java
+++ src/main/java/com/seulseul/seulseul/dto/endPos/EndPosDto.java
@@ -13,10 +13,10 @@ import lombok.*;
 @AllArgsConstructor //모든 필드를 파라미터로 받는 생성자 자동 생성
 public class EndPosDto {
 
-    @JsonProperty("endx")
+    @JsonProperty("endX")
     private double endX;
 
-    @JsonProperty("endy")
+    @JsonProperty("endX")
     private double endY;
 
     @JsonProperty("endNickName")
