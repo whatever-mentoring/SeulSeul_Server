f34a686bfabe00a26dd14bf8ff51b5dc1fea6064
jooda00 <dragon2000a@gmail.com>
2023-09-18 17:07:14 +0900

    feat: findByUuid
    - 헤더로 유저 식별을 위한 메소드 추가

--- src/main/java/com/seulseul/seulseul/service/user/UserService.java
+++ src/main/java/com/seulseul/seulseul/service/user/UserService.java
@@ -1,11 +1,15 @@
 package com.seulseul.seulseul.service.user;
 
+import com.seulseul.seulseul.config.CustomException;
+import com.seulseul.seulseul.config.ErrorCode;
 import com.seulseul.seulseul.dto.user.UserDto;
 import com.seulseul.seulseul.entity.user.User;
 import com.seulseul.seulseul.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,13 @@ public class UserService {
     @Transactional(readOnly = false)
     public User saveUser(UserDto userDto) {
         return userRepository.save(new User(userDto.getUuid()));
+    }
+
+    public User getUserByUuid(UUID uuid) {
+        User user = userRepository.findByUuid(uuid);
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        return user;
     }
 }
