ed537520b002bb28d02fd44f658d22bda2505c9d
jooda00 <dragon2000a@gmail.com>
2023-09-23 02:11:55 +0900

    feat: 목적지 관련 에러코드 추가

--- src/main/java/com/seulseul/seulseul/config/ErrorCode.java
+++ src/main/java/com/seulseul/seulseul/config/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     CONFLICT(409, "요청한 리소스가 서버와 충돌합니다."),
     USER_NOT_FOUND(410, "유저가 존재하지 않습니다."),
     BASEROUTE_NOT_FOUND(411, "저장된 경로가 없습니다."),
-    ALARM_NOT_FONUD(412, "알림 시간 및 간격이 세팅되어 있지 않습니다.");
+    ALARM_NOT_FONUD(412, "알림 시간 및 간격이 세팅되어 있지 않습니다."),
+    ENDPOS_NOT_FOUND(413, "현재 사용자가 저장한 목적지가 아닙니다.");
 
     private final int code;
     private final String message;
