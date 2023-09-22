bd81964f6c88d1952dd6c2bd15066ab396d1dea7
seoyeon0201 <psy0201123@gmail.com>
2023-09-22 14:58:29 +0900

    feat: if route is change,delete original stopTime

--- src/main/java/com/seulseul/seulseul/repository/stopTimeList/StopTimeListRepository.java
+++ src/main/java/com/seulseul/seulseul/repository/stopTimeList/StopTimeListRepository.java
@@ -3,5 +3,8 @@ package com.seulseul.seulseul.repository.stopTimeList;
 import com.seulseul.seulseul.entity.stopTimeList.StopTimeList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StopTimeListRepository extends JpaRepository<StopTimeList, Long> {
+    List<StopTimeList> findByBaseRouteId(Long baseRouteId);
 }
