5ebbac221ac16f5f4ba8a13460e19ef98ff15cb2
jooda00 <dragon2000a@gmail.com>
2023-09-28 03:53:04 +0900

    feat: transfer -> stopTimeList를 service로 변경
    - /v1/route/detail로 controller 합병 완료

--- src/main/java/com/seulseul/seulseul/controller/android/RouteDetailController.java
+++ src/main/java/com/seulseul/seulseul/controller/android/RouteDetailController.java
@@ -12,6 +12,8 @@ import com.seulseul.seulseul.repository.user.UserRepository;
 import com.seulseul.seulseul.service.alarm.AlarmService;
 import com.seulseul.seulseul.service.android.RouteDetailService;
 import com.seulseul.seulseul.service.baseRoute.BaseRouteService;
+import com.seulseul.seulseul.service.result.ComputeResultService;
+import com.seulseul.seulseul.service.result.UpdateResultService;
 import com.seulseul.seulseul.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,24 +33,24 @@ public class RouteDetailController {
     private final RouteDetailService routeDetailService;
     private final BaseRouteService baseRouteService;
     private final UserService userService;
+    private final UpdateResultService updateResultService;
+    private final ComputeResultService computeResultService;
 
     @GetMapping("/v1/route/detail")
     public ResponseEntity<ResponseData> routeDetail(@RequestHeader("Auth") UUID uuid) throws ParseException, IOException {
         User user = userService.getUserByUuid(uuid);
         BaseRoute baseRoute = baseRouteService.findByUser(user);
-
-        //baseRoute에 존재하는 데이터 전달
-        RouteDetailDto routeDetailDto = routeDetailService.routeDetailFromBaseRoute(baseRoute.getId());
-
-        //실제 시간 계산 로직 => 뒤에서부터 확인
-        List<String> timeList = routeDetailService.compute(baseRoute.getId());
-
-        //재확인 => 목적지 역 이전에 먼저 끊기는 역이 존재하는 경우 해당 시간에 맞춰 timeList 변경
-        String timeList2 = routeDetailService.checkTimeList(baseRoute.getId(), timeList);
-        System.out.println("/v1/route/detail timeList2: "+timeList2);   //여기까지 성공
-
-        //시간 업데이트
-        routeDetailService.updateTimeList(baseRoute.getId(), routeDetailDto, timeList2);
+        RouteDetailDto routeDetailDto = new RouteDetailDto();
+        // 첫번째 작동 시
+        if (baseRoute.getSID() == 0 && baseRoute.getEID() == 0) {
+            System.out.println("first");
+            routeDetailDto = computeResultService.computeTime(baseRoute.getId());
+        }
+        // 출발지나 도착지 좌표가 변경되어 작동 시
+        else {
+            System.out.println("changed");
+            routeDetailDto = updateResultService.getUpdatedResult(baseRoute.getId());
+        }
         RouteDetailWrapDto wrapDto = new RouteDetailWrapDto();
         // RouteDetail DB에 저장(User와 1:1 매핑)
         RouteDetail routeDetail = routeDetailService.saveRouteDetail(routeDetailDto, user);
