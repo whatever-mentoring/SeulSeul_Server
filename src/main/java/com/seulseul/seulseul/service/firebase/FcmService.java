a3d2f81dea5ed1e529be70e662022b1270754ddc
jooda00 <dragon2000a@gmail.com>
2023-09-29 01:42:58 +0900

    feat: fcm

--- src/main/java/com/seulseul/seulseul/service/firebase/FcmService.java
+++ src/main/java/com/seulseul/seulseul/service/firebase/FcmService.java
@@ -13,6 +13,10 @@ import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.*;
 import java.util.concurrent.ScheduledFuture;
 
 @Service
@@ -26,8 +30,33 @@ public class FcmService {
     // 상태 변수 추가
     private boolean stopScheduling = false;
 
+    public static String[] extractTimes(String input) {
+        String cleaned = input.replace("\"[\\\"", "").replace("\\\"]\"", "").replace("\\\",\\\"", ",");
+        String[] times = cleaned.split(",");
+        return times;
+    }
+
     public void schedule(BaseRoute baseRoute) {
-        String cronExpression = convertToCron(baseRoute.getAlarm().getAlarmTerm()); // DB에서 alarmTerm을 가져와서 cron 표현식으로 변환
+        long alarmTime = baseRoute.getAlarm().getAlarmTime();
+        int alarmTerm = baseRoute.getAlarm().getAlarmTerm();
+        String[] timeList = extractTimes(baseRoute.getRouteDetail().getTimeList());
+        int timeSize = timeList.length;
+        // 마지막 시간에서 알람시간을 뺀 시간부터 알람 시작
+        String lastTime = timeList[timeSize - 2];
+        LocalTime startTime;
+        if ("24".equals(lastTime.split(":")[0])) {
+            String correctedLastTime = "00" + lastTime.substring(2);
+            startTime = LocalTime.parse(correctedLastTime).plusHours(1).minusMinutes(alarmTime);
+        } else {
+            startTime = LocalTime.parse(lastTime).minusMinutes(alarmTime);
+        }
+        System.out.println(startTime);
+
+        while (startTime.isBefore(LocalTime.now())) {
+            startTime = startTime.plusMinutes(alarmTerm);
+        }
+
+        String cronExpression = convertToCron(alarmTerm); // DB에서 alarmTerm을 가져와서 cron 표현식으로 변환
 
         CronTrigger cronTrigger = new CronTrigger(cronExpression);
 
@@ -38,13 +67,13 @@ public class FcmService {
         Runnable task = () -> {
             try {
                 // DB에서 최신 정보 조회
-                BaseRoute latestBaseRoute = baseRouteRepository.findById(baseRoute.getId())
+                BaseRoute latestBaseRate = baseRouteRepository.findById(baseRoute.getId())
                         .orElseThrow(() -> new CustomException(ErrorCode.BASEROUTE_NOT_FOUND));
-                if (latestBaseRoute != null && latestBaseRoute.getAlarm().isAlarmEnabled()) {
+                if(latestBaseRate!=null && latestBaseRate.getAlarm().isAlarmEnabled()){
                     System.out.println("notice");
-                    String result = notificationService.sendToken(latestBaseRoute); // FCM 메시지 전송
-                    if ("알림을 모두 보냈습니다.".equals(result)) {
-                        stopScheduling = true;  // 상태 변수 업데이트
+                    String result = notificationService.sendToken(latestBaseRate); // FCM 메시지 전송
+                    if("알림을 모두 보냈습니다.".equals(result)){
+                        stopScheduling=true;  // 상태 변수 업데이트
                     }
                 }
             } catch (FirebaseMessagingException e) {
@@ -62,7 +91,7 @@ public class FcmService {
         scheduledFuture=this.taskScheduler.schedule(task,(Trigger)cronTrigger);
     }
 
-    // alarmTerm을 cron에 넣어주기
+    // alarmTerm을 cron에 변수로 넣음
     private String convertToCron(int minutesInterval){
 //       return "* /" + minutesInterval + " * * * *";
         return "0 0/" + minutesInterval + " * * * ?";
