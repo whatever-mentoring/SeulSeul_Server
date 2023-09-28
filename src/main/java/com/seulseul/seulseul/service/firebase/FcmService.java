3ee046fe04edd2955dac27c5e07c6c9a6b4ef75e
jooda00 <dragon2000a@gmail.com>
2023-09-28 22:13:36 +0900

    feat: fcm

--- src/main/java/com/seulseul/seulseul/service/firebase/FcmService.java
+++ src/main/java/com/seulseul/seulseul/service/firebase/FcmService.java
@@ -1,19 +1,18 @@
 package com.seulseul.seulseul.service.firebase;
 
+
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.seulseul.seulseul.config.CustomException;
 import com.seulseul.seulseul.config.ErrorCode;
 import com.seulseul.seulseul.entity.baseRoute.BaseRoute;
 import com.seulseul.seulseul.repository.baseRoute.BaseRouteRepository;
+import com.seulseul.seulseul.service.firebase.NotificationService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.concurrent.ScheduledFuture;
 
 @Service
@@ -23,6 +22,9 @@ public class FcmService {
     private final NotificationService notificationService;
     private ScheduledFuture<?> scheduledFuture;
     private final BaseRouteRepository baseRouteRepository;
+
+    // 상태 변수 추가
+    private boolean stopScheduling = false;
 
     public void schedule(BaseRoute baseRoute) {
         String cronExpression = convertToCron(baseRoute.getAlarm().getAlarmTerm()); // DB에서 alarmTerm을 가져와서 cron 표현식으로 변환
@@ -39,19 +41,30 @@ public class FcmService {
                 BaseRoute latestBaseRoute = baseRouteRepository.findById(baseRoute.getId())
                         .orElseThrow(() -> new CustomException(ErrorCode.BASEROUTE_NOT_FOUND));
                 if (latestBaseRoute != null && latestBaseRoute.getAlarm().isAlarmEnabled()) {
-                    notificationService.sendToken(latestBaseRoute); // FCM 메시지 전송
+                    System.out.println("notice");
+                    String result = notificationService.sendToken(latestBaseRoute); // FCM 메시지 전송
+                    if ("알림을 모두 보냈습니다.".equals(result)) {
+                        stopScheduling = true;  // 상태 변수 업데이트
+                    }
                 }
             } catch (FirebaseMessagingException e) {
                 e.printStackTrace();
             }
+
+            // 작업 후 상태 확인하여 스케줄링 중지
+            if(stopScheduling){
+                scheduledFuture.cancel(false);
+                System.out.println("Scheduled Task is stopped.");
+                stopScheduling=false;  //상태 초기화
+            }
         };
 
-        scheduledFuture = this.taskScheduler.schedule(task, (Trigger) cronTrigger);
-
+        scheduledFuture=this.taskScheduler.schedule(task,(Trigger)cronTrigger);
     }
 
     // alarmTerm을 cron에 넣어주기
-    private String convertToCron(int minutesInterval) {
+    private String convertToCron(int minutesInterval){
+//       return "* /" + minutesInterval + " * * * *";
         return "0 0/" + minutesInterval + " * * * ?";
     }
 }
--- src/main/java/com/seulseul/seulseul/service/firebase/NotificationService.java
+++ src/main/java/com/seulseul/seulseul/service/firebase/NotificationService.java
@@ -4,46 +4,87 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.seulseul.seulseul.entity.TokenKey;
+import com.seulseul.seulseul.entity.android.RouteDetail;
 import com.seulseul.seulseul.entity.baseRoute.BaseRoute;
 import com.seulseul.seulseul.service.baseRoute.BaseRouteService;
 import com.seulseul.seulseul.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     private final UserService userService;
     private final BaseRouteService baseRouteService;
+    private int cnt = 0;
+    private int gap;
+    public static String[] extractTimes(String input) {
+        String cleaned = input.replace("\"[\\\"", "").replace("\\\"]\"", "").replace("\\\",\\\"", ",");
+        String[] times = cleaned.split(",");
+        return times;
+    }
 
     public String sendToken(BaseRoute baseRoute) throws FirebaseMessagingException {
         String pos = baseRoute.getFirstStation();
         Long original = baseRoute.getAlarm().getAlarmTime();
         String alarmTime = "";
+//        RouteDetail routeDetail = baseRoute.getRouteDetail();
+//
+//        String[] timeList = extractTimes(routeDetail.getTimeList());
+//        int timeSize = timeList.length;
+
+//        String lastTime = timeList[timeSize-1];
+//        String stringLastTime = "20:00";
+        // String -> LocalTime
+//        LocalTime time = LocalTime.parse(stringLastTime);
 
         if (original >= 60) {
-            if (original%60 == 0) {
-                alarmTime = original/60 +"시간 " + "00분";
+            if (original % 60 == 0) {
+                alarmTime = original / 60 + "시간 " + "00분";
             } else {
-                alarmTime = original/60 +"시간 " + original%60 + "분";
+                alarmTime = original / 60 + "시간 " + original % 60 + "분";
             }
         } else {
             alarmTime = original + "분";
         }
+//        int hours = Integer.parseInt(alarmTime.split("시간")[0].trim());
+//        int minutes = Integer.parseInt(alarmTime.split("분")[0].trim());
+//
+////        LocalTime term = LocalTime.of(hours, minutes);
+//        String formattedTime = String.format("%d:%02d", hours, minutes);
+//        LocalTime stan = LocalTime.parse(stringLastTime);
+//        LocalTime term = LocalTime.parse(formattedTime);
 
-        String body = "마지막 위치 "+ pos + "역을 기준으로 "+ alarmTime +" 뒤에 막차가 끊깁니다!";
 
-        // See documentation on defining a message payload.
-        Message message = Message.builder()
-                .putData("title", "SeulSeul")
-                .putData("body", body)
-                .setToken(baseRoute.getUser().getToken())
-                .build();
+        int alarmTerm = baseRoute.getAlarm().getAlarmTerm();
+        int stringToIntAlarmTime = original.intValue();
+        gap = stringToIntAlarmTime / alarmTerm;
+        System.out.println(gap);
+        if (cnt <= gap) {
+            String body = "마지막 위치 "+ pos + "역을 기준으로 "+ (stringToIntAlarmTime-alarmTerm*cnt) +" 뒤에 막차가 끊깁니다!";
 
-        // Send a message to the device corresponding to the provided
-        // registration token.
-        String response = FirebaseMessaging.getInstance().send(message);
+            // See documentation on defining a message payload.
+            Message message = Message.builder()
+                    .putData("title", "SeulSeul")
+                    .putData("body", body)
+                    .setToken(baseRoute.getUser().getToken())
+                    .build();
 
-        return response;
+            String response = FirebaseMessaging.getInstance().send(message);
+
+            cnt += 1;
+
+            if (cnt > gap) {
+                return "알림을 모두 보냈습니다.";
+            }
+
+            System.out.println("new" + cnt);
+
+            return response;
+        }
+        return "알림을 모두 보냈습니다.";
     }
+
 }
