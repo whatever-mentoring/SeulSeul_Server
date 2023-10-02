6d17b186091f5d6c5258c37a9417ce14c0706c78
jooda00 <dragon2000a@gmail.com>
2023-10-02 14:33:21 +0900

    fix: config

--- src/main/java/com/seulseul/seulseul/controller/firbase/FirebaseConfig.java
+++ src/main/java/com/seulseul/seulseul/controller/firbase/FirebaseConfig.java
@@ -3,31 +3,60 @@ package com.seulseul.seulseul.controller.firbase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 @Configuration
 public class FirebaseConfig {
-    @Value("classpath:firebase/service-account.json")
-    private Resource resource;
+//    @Value("classpath:firebase/service-account.json")
+//    private Resource resource;
+//
+//    @PostConstruct
+//    public void initFirebase() {
+//        try {
+//            // Service Account를 이용하여 Fireabse Admin SDK 초기화
+//            InputStream serviceAccount = resource.getInputStream();
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//            FirebaseApp.initializeApp(options);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource("firebase/service-account.json");
+        InputStream refreshToken = resource.getInputStream();
 
-    @PostConstruct
-    public void initFirebase() {
-        try {
-            // Service Account를 이용하여 Fireabse Admin SDK 초기화
-            InputStream serviceAccount = resource.getInputStream();
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            e.printStackTrace();
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if(firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList) {
+                if (firebaseApp.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
         }
+        else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
